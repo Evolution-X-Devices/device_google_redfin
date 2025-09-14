f7bb01a7f46180363666ed2bc3d9e9304234789a
Apelete Seketeli <apelete@seketeli.net>
2025-09-14 23:29:11 +0200

    redfin: Allow files to live in specified root path

--- lineage_redfin.mk
+++ lineage_redfin.mk
@@ -29,4 +29,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 TARGET_USES_MINI_GAPPS := true
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/GoogleExtShared/GoogleExtShared.apk \
+    system/app/GooglePrintRecommendationService/GooglePrintRecommendationService.apk \
+    system/etc/permissions/privapp-permissions-google.xml \
+    system/priv-app/DocumentsUIGoogle/DocumentsUIGoogle.apk \
+    system/priv-app/TagGoogle/TagGoogle.apk
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/priv-app/OmniStyle/OmniStyle.apk
+
 $(call inherit-product, vendor/google/redfin/redfin-vendor.mk)
